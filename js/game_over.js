e668b8b53365cf6a4f9ba35bb8879736431010b7
Manuel Fernandez <manfergo25@gmail.com>
2015-04-23 16:59:16 -0430

    Add click

--- js/game_over.js
+++ js/game_over.js
@@ -5,7 +5,7 @@ var GameOver = {
 
 	create: function () {
       this.game.add.image(40,0,'game-over');
-      stateText = game.add.text(game.world.centerX,0,'PRESIONE ESPACIO PARA INICIAR', { font: '32px Arial', fill: '#fff' });
+      stateText = game.add.text(game.world.centerX,0,'PRESIONE ESPACIO O CLICK PARA INICIAR', { font: '32px Arial', fill: '#fff' });
       stateText.anchor.setTo(0.5, 0.5);
       stateText.visible = true;
       game.add.tween(stateText).to( { y: 445 }, 2400, Phaser.Easing.Bounce.Out, true);
@@ -15,7 +15,7 @@ var GameOver = {
 	},
 
 	update: function () {
-        if (this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+        if (this.game.input.activePointer.isDown || this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
           music.stop();      
           this.startGame();
         }
--- js/main_menu.js
+++ js/main_menu.js
@@ -5,7 +5,7 @@ var MainMenu = {
 
 	create: function () {
       this.game.add.image(0,0,'main_menu');
-      stateText = game.add.text(game.world.centerX,0,'PRESIONE ESPACIO PARA JUGAR', { font: '32px Arial', fill: '#fff' });
+      stateText = game.add.text(game.world.centerX,0,'PRESIONE ESPACIO O CLICK PARA JUGAR', { font: '32px Arial', fill: '#fff' });
       stateText.anchor.setTo(0.5, 0.5);
       stateText.visible = true;
       game.add.tween(stateText).to( { y: 445 }, 2400, Phaser.Easing.Bounce.Out, true);
@@ -15,7 +15,7 @@ var MainMenu = {
 	},
 
 	update: function () {
-        if (this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+        if (this.game.input.activePointer.isDown || this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
           music.stop();      
           this.startGame();
         }
