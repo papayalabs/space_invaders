09ca4c94c6aac8d1d9b3b29383bfe6356e8bf460
Manuel Fernandez <manfergo25@gmail.com>
2015-04-29 16:23:47 -0430

    Resize mobile

--- js/boot.js
+++ js/boot.js
@@ -6,25 +6,54 @@ var Boot = {
 	},
 	
 	create: function () {
-		this.input.maxPointers = 3;
-        this.stage.disableVisibilityChange = true;
+		this.input.maxPointers = 1;
+	        this.stage.disableVisibilityChange = true;
 
-        if (this.game.device.desktop)
-        {
-            this.scale.pageAlignHorizontally = true;
-        }
-        else
-        {
-            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            this.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
-            this.scale.minWidth = 320;
-            this.scale.minHeight = 180;
-            this.scale.forceLandscape = true;
-            this.scale.pageAlignHorizontally = true;
-            this.scale.setScreenSize(true);
-        }
+	        if (this.game.device.desktop)
+	        {
+	            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+	            this.scale.setMinMax(480, 260, 1024, 768);
+	            this.scale.pageAlignHorizontally = true;
+	            this.scale.pageAlignVertically = true;
+	        }
+	        else
+	        {
+	            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+	            this.scale.setMinMax(480, 260, 1024, 768);
+	            this.scale.pageAlignHorizontally = true;
+	            this.scale.pageAlignVertically = true;
+	            this.scale.forceOrientation(true, false);
+	            this.scale.setResizeCallback(this.gameResized, this);
+	            this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
+	            this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
+	        }
+
 
         this.state.start('Preloader');
-    }
+    },
+
+	gameResized: function (width, height) {
+
+	        //  This could be handy if you need to do any extra processing if the game resizes.
+	        //  A resize could happen if for example swapping orientation on a device or resizing the browser window.
+	        //  Note that this callback is only really useful if you use a ScaleMode of RESIZE and place it inside your main game state.
+
+	    },
+
+	    enterIncorrectOrientation: function () {
+
+	        BasicGame.orientated = false;
+
+	        document.getElementById('orientation').style.display = 'block';
+
+	    },
+
+	    leaveIncorrectOrientation: function () {
+
+	        BasicGame.orientated = true;
+
+	        document.getElementById('orientation').style.display = 'none';
+
+	    }
 	
 };
